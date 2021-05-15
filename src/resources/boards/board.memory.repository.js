81e6682bfbe342cab150d4951b1ad9d376218b7d
96tm <khabcity@gmail.com>
2021-05-15 22:48:01 +0300

    feat: add boards directory and template files, add boards route

---
+++ src/resources/boards/board.memory.repository.js
@@ -0,0 +1,16 @@
+const Board = require('./board.model');
+
+const boards = [
+  new Board({ title: 'Board1', columns: [] }),
+  new Board({ title: 'Board2', columns: [] }),
+  new Board({ title: 'Board3', columns: [] }),
+  new Board({ title: 'Board4', columns: [] }),
+  new Board({ title: 'Board5', columns: [] }),
+  new Board({ title: 'Board6', columns: [] })
+];
+
+const getAll = async () => {
+  return boards;
+};
+
+module.exports = { getAll };
---
+++ src/resources/boards/board.router.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const boardsService = require('./board.service');
+
+router.route('/').get(async (req, res) => {
+  const boards = await boardsService.getAll();
+  res.json(boards);
+});
+
+module.exports = router;
