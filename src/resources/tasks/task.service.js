b7f649cad5f916842a79bd8d5edb7d7b564fe0c4
96tm <khabcity@gmail.com>
2021-05-27 23:42:57 +0300

    feat: add jsdoc annotations to src/tasks/task.service.js

--- src/resources/tasks/task.service.js
+++ src/resources/tasks/task.service.js
@@ -1,18 +1,105 @@
 const tasksRepo = require('./task.memory.repository');
 
-const getAllByUserId = id => tasksRepo.getAllByUserId(id);
-const getAllByBoardId = id => tasksRepo.getAllByBoardId(id);
-const add = ({ title, order, description, userId, boardId, columnId }) => tasksRepo.add({
+/**
+ * Get all tasks with given userId
+ *
+ * @param {string} id - user id
+ *
+ * @return {Task[]} - array of tasks
+ *
+ * @example
+ *
+ *  getAllByUserId('1')
+ */
+const getAllByUserId = (id) => tasksRepo.getAllByUserId(id);
+
+/**
+ * Get all tasks with given boardId
+ *
+ * @param {string} id - board id
+ *
+ * @return {Task[]} - array of tasks
+ *
+ * @example
+ *
+ *  getAllByBoardId('1')
+ */
+const getAllByBoardId = (id) => tasksRepo.getAllByBoardId(id);
+
+/**
+ * Create task with given parameters,
+ * add it into database and return it
+ *
+ * @param {string} task.title - task title
+ * @param {string} task.order - task order
+ * @param {string} task.description - task description
+ * @param {string} task.userId - id of a user to whom the task will be assigned
+ * @param {string} task.boardId - id of board into which the task will be added
+ * @param {string} task.columnId - id of column into which the task will be added
+ *
+ * @return {Task} - created task
+ *
+ * @example
+ *
+ *  add('Task title', '1', 'Task description', '1', '1', '1')
+ */
+const add = ({ title, order, description, userId, boardId, columnId }) =>
+  tasksRepo.add({
     title,
     order,
     description,
     userId,
     boardId,
-    columnId
+    columnId,
   });
-const deleteById = id => tasksRepo.deleteById(id);
+
+/**
+ * Find task with given id and
+ * delete it from database
+ *
+ * @param {string} id - task id
+ *
+ * @example
+ *
+ *  deleteById('1')
+ */
+const deleteById = (id) => tasksRepo.deleteById(id);
+
+/**
+ * Get task with given boardId and userId and return it;
+ * return undefined if nothing found
+ *
+ * @param {string} boardId - board id
+ * @param {string} taskId - task id
+ *
+ * @return {task|undefined} - found task or undefined
+ *
+ * @example
+ *
+ *  getByBoardAndTaskId('1', '1')
+ */
 const getByBoardAndTaskId = (boardId, taskId) =>
   tasksRepo.getByBoardAndTaskId(boardId, taskId);
+
+/**
+ * Find task by given boardId and taskId,
+ * then edit it using given parameters and return edited task;
+ * return undefined if nothing found
+ *
+ * @param {string} boardId - id of board where the task belongs
+ * @param {string} taskId - id of a task to be edited
+ * @param {string} title - task title
+ * @param {string} order - task order
+ * @param {string} description - task description
+ * @param {string} userId - id of a user to whom the task will be assigned
+ * @param {string} columnId - id of column into which the task will be added
+ *
+ * @return {Task|undefined} - edited task or undefined
+ *
+ * @example
+ *
+ *  editByBoardAndTaskId('1', '1', 'Task title', '1', 'Task description', '1', '1')
+ */
 const editByBoardAndTaskId = async ({
   boardId,
   taskId,
@@ -20,15 +107,16 @@ const editByBoardAndTaskId = async ({
   order,
   description,
   userId,
-  columnId
-}) => tasksRepo.editByBoardAndTaskId({
+  columnId,
+}) =>
+  tasksRepo.editByBoardAndTaskId({
     boardId,
     taskId,
     title,
     order,
     description,
     userId,
-    columnId
+    columnId,
   });
 
 module.exports = {
@@ -37,5 +125,5 @@ module.exports = {
   editByBoardAndTaskId,
   deleteById,
   add,
-  getAllByUserId
+  getAllByUserId,
 };
