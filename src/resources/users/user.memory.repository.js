ab72b8bf6d03102759ea55e3845bfe55ffb262fb
96tm <khabcity@gmail.com>
2021-05-28 11:37:18 +0300

    feat: add jsdoc annotations to src/resources/users/user.memory.repository.js

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,23 +1,61 @@
-const User = require('./user.model');
+const users = [];
 
-const users = [
-  new User({ id: '1', name: 'John', login: 'john', password: '12345' }),
-  new User({ name: 'Dan', login: 'dan', password: '12345' }),
-  new User({ name: 'Kelly', login: 'kelly', password: '12345' }),
-  new User({ name: 'Sam', login: 'sam', password: '12345' }),
-  new User({ name: 'Ann', login: 'ann', password: '12345' }),
-  new User({ name: 'Jim', login: 'jim', password: '12345' })
-];
-
+/**
+ * Get all users in the database
+ *
+ * @return {User[]} - all users
+ *
+ * @example
+ *
+ *  getAll()
+ */
 const getAll = async () => users;
 
-const getUserById = async id => users.find(user => user.id === id);
+/**
+ * Find and return user with given id;
+ * return undefined if not found
+ *
+ * @param {string} id - user id
+ *
+ * @return {User|undefined} - found user or undefined
+ *
+ * @example
+ *
+ *  getUserById('1')
+ */
+const getUserById = async (id) => users.find((user) => user.id === id);
 
-const addUser = async user => {
+/**
+ * Add given user to database, then return the user
+ *
+ * @param {User} user - user to be added to the database
+ * @return {User} - added user
+ *
+ * @example
+ *
+ *  addUser(new User('username', 'login', 'password'))
+ */
+const addUser = async (user) => {
   users.push(user);
   return user;
 };
 
+/**
+ * Find user with given id,
+ * edit found object using given parameters
+ * and return found user;
+ * if not found, return undefined
+ *
+ * @param {string} id - user id
+ * @param {string} name - user name
+ * @param {string} login - user login
+ * @param {string} password - user password
+ * @return {User|undefined} - edited user or undefined
+ *
+ * @example
+ *
+ *  editUser('1', 'new name', 'new login', 'new password')
+ */
 const editUser = async (id, name, login, password) => {
   const user = await getUserById(id);
   if (user) {
@@ -28,11 +66,23 @@ const editUser = async (id, name, login, password) => {
   return user;
 };
 
-const deleteUser = async id => {
+/**
+ * Find user with given id,
+ * delete and return found object;
+ * return undefined if not found
+ *
+ * @param {string} id - user id
+ * @return {User|undefined} - deleted useror undefined
+ *
+ * @example
+ *
+ *  deleteUser('1')
+ */
+const deleteUser = async (id) => {
   const userToDelete = await getUserById(id);
   if (userToDelete) {
     users.splice(
-      users.findIndex(user => user.id === id),
+      users.findIndex((user) => user.id === id),
       1
     );
   }
