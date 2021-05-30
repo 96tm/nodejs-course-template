66d18a994dcd658fcc505f33fb1b7c010400ad34
96tm <khabcity@gmail.com>
2021-05-30 19:22:48 +0300

    feat: finish task 4 part 1

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,66 +1,14 @@
 const boardsRepo = require('./board.memory.repository');
 
-/**
- * Get all boards
- *
- * @example
- *
- *  getAll()
- */
 const getAll = () => boardsRepo.getAll();
 
-/**
- * Add given board to database
- *
- * @param {Board} board - Board to add
- *
- * @example
- *
- *  addBoard(board)
- */
 const addBoard = (board) => boardsRepo.addBoard(board);
 
-/**
- * Find board with given id and return it; if nothing found, return undefined.
- *
- * @param {string} id - board id
- *
- * @return {Board | undefined} - found Board or undefined
- *
- * @example
- *
- *  getById('1')
- */
 const getById = (boardId) => boardsRepo.getById(boardId);
 
-/**
- * Find board with given id,
- * set its title and columns to given values
- * and return the board;
- * if nothing found, return undefined
- *
- * @param {String} id - board id
- * @param {String} title - new title
- * @param {Column[]} columns - array of columns
- * @return {Board|undefined} - found board or undefined
- *
- * @example
- *
- *  editById('1', 'New title', [ column0, column1 ])
- */
 const editById = (id, title, columns) =>
   boardsRepo.editById(id, title, columns);
 
-/**
- * Delete board with given id and return it; if nothing found, return undefined
- *
- * @param {string} id - board id
- * @return {Board|undefined} - found board or undefined
- *
- * @example
- *
- *  deleteById('1')
- */
 const deleteById = (id) => boardsRepo.deleteById(id);
 
 module.exports = { getAll, addBoard, getById, editById, deleteById };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,73 +1,14 @@
 const usersRepo = require('./user.memory.repository');
 
-/**
- * Get all users in the database
- *
- * @return {User[]} - all users
- *
- * @example
- *
- *  getAll()
- */
 const getAll = () => usersRepo.getAll();
 
-/**
- * Find and return user with given id;
- * return undefined if not found
- *
- * @param {string} id - user id
- *
- * @return {User|undefined} - found user or undefined
- *
- * @example
- *
- *  getUserById('1')
- */
 const getUserById = (id) => usersRepo.getUserById(id);
 
-/**
- * Add given user to database, then return the user
- *
- * @param {User} user - user to be added to the database
- * @return {User} - added user
- *
- * @example
- *
- *  addUser(new User('username', 'login', 'password'))
- */
 const addUser = (user) => usersRepo.addUser(user);
 
-/**
- * Find user with given id,
- * edit found object using given parameters
- * and return found user;
- * if not found, return undefined
- *
- * @param {string} id - user id
- * @param {string} name - user name
- * @param {string} login - user login
- * @param {string} password - user password
- * @return {User|undefined} - edited user or undefined
- *
- * @example
- *
- *  editUser('1', 'new name', 'new login', 'new password')
- */
 const editUser = (id, name, login, password) =>
   usersRepo.editUser(id, name, login, password);
 
-/**
- * Find user with given id,
- * delete and return found object;
- * return undefined if not found
- *
- * @param {string} id - user id
- * @return {User|undefined} - deleted useror undefined
- *
- * @example
- *
- *  deleteUser('1')
- */
 const deleteUser = (id) => usersRepo.deleteUser(id);
 
 module.exports = { getAll, getUserById, addUser, editUser, deleteUser };
