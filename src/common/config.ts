03465579217260382929bab360c8435d47079962
96tm <khabcity@gmail.com>
2021-06-13 22:48:02 +0300

    fix: change port back

--- src/common/config.ts
+++ src/common/config.ts
@@ -5,7 +5,7 @@ dotenv.config({
   path: path.join(__dirname, '../../.env'),
 });
 
-const PORT = process.env['HOST_PORT'];
+const PORT = process.env['PORT'];
 const NODE_ENV = process.env['NODE_ENV'];
 const MONGO_CONNECTION_STRING = process.env['MONGO_CONNECTION_STRING'];
 const JWT_SECRET_KEY = process.env['JWT_SECRET_KEY'];
